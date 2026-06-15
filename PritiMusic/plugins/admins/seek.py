6eaa561f9c30f773ac1f74d97e07392ec74bf87c
THE SHIV <shivofficial004@gmail.com>
2026-06-15 14:50:20 +0530

    Update seek.py

--- PritiMusic/plugins/admins/seek.py
+++ PritiMusic/plugins/admins/seek.py
@@ -1,13 +1,14 @@
-from pyrogram import filters
+import asyncio
+from pyrogram import filters, Client
 from pyrogram.types import Message
 
 from PritiMusic import YouTube, app
 from PritiMusic.core.call import Lucky
 from PritiMusic.misc import db
-from PritiMusic.utils import AdminRightsCheck, seconds_to_min
+from PritiMusic.utils.formatters import seconds_to_min
+from PritiMusic.cplugin.utils.decorators.admins import AdminRightsCheck
 from PritiMusic.utils.inline import close_markup
 from config import BANNED_USERS
-
 
 @app.on_message(
     filters.command(["seek", "cseek", "seekback", "cseekback"])
@@ -22,68 +23,67 @@ async def seek_comm(cli, message: Message, _, chat_id):
     query = message.text.split(None, 1)[1].strip()
     if not query.isnumeric():
         return await message.reply_text(_["admin_21"])
-        
+    
+    # Queue check
     playing = db.get(chat_id)
     if not playing:
         return await message.reply_text(_["queue_2"])
         
-    # 🚀 Fix: KeyError ('seconds') se bachne ke liye safe get() use kiya
+    # Duration variables
     duration_seconds = int(playing[0].get("seconds", 0))
+    duration_played = int(playing[0].get("played", 0))
+    duration_to_skip = int(query)
+    duration_str = playing[0].get("dur", "0:00")
+    
     if duration_seconds == 0:
         return await message.reply_text(_["admin_22"])
         
-    file_path = playing[0]["file"]
+    # Command type detection (cseek/cseekback = backward)
+    is_backward = message.command[0].startswith("c")
     
-    # 🚀 Fix: KeyError ('played') se bachne ke liye safe get() use kiya
-    duration_played = int(playing[0].get("played", 0))
-    duration_to_skip = int(query)
-    duration = playing[0]["dur"]
-    
-    if message.command[0][-2] == "c":
-        if (duration_played - duration_to_skip) <= 10:
-            return await message.reply_text(
-                text=_["admin_23"].format(seconds_to_min(duration_played), duration),
-                reply_markup=close_markup(_),
-            )
-        to_seek = duration_played - duration_to_skip + 1
+    # Calculate target time
+    if is_backward:
+        to_seek = duration_played - duration_to_skip
+        if to_seek < 0: to_seek = 0
     else:
-        if (duration_seconds - (duration_played + duration_to_skip)) <= 10:
-            return await message.reply_text(
-                text=_["admin_23"].format(seconds_to_min(duration_played), duration),
-                reply_markup=close_markup(_),
-            )
-        to_seek = duration_played + duration_to_skip + 1
-        
+        to_seek = duration_played + duration_to_skip
+        if to_seek > duration_seconds: to_seek = duration_seconds - 5
+    
+    # Safety: Start ya End ke bahut close ho toh skip na karein
+    if abs(duration_played - to_seek) < 10:
+         return await message.reply_text(
+            text=_["admin_23"].format(seconds_to_min(duration_played), duration_str),
+            reply_markup=close_markup(_),
+        )
+
     mystic = await message.reply_text(_["admin_24"])
     
+    # File path setup
+    file_path = playing[0].get("file", "")
     if "vid_" in file_path:
         n, file_path = await YouTube.video(playing[0]["vidid"], True)
-        if n == 0:
-            return await message.reply_text(_["admin_22"])
+        if n == 0: return await message.reply_text(_["admin_22"])
             
-    check = (playing[0]).get("speed_path")
-    if check:
-        file_path = check
+    speed_path = playing[0].get("speed_path")
+    if speed_path: file_path = speed_path
         
     if "index_" in file_path:
         file_path = playing[0]["vidid"]
         
     try:
+        # Seek stream
         await Lucky.seek_stream(
             chat_id,
             file_path,
             seconds_to_min(to_seek),
-            duration,
+            duration_str,
             playing[0]["streamtype"],
         )
-    except:
+    except Exception as e:
         return await mystic.edit_text(_["admin_26"], reply_markup=close_markup(_))
         
-    # 🚀 Fix: Bina error trigger kiye safe assignment
-    if message.command[0][-2] == "c":
-        db[chat_id][0]["played"] = duration_played - duration_to_skip
-    else:
-        db[chat_id][0]["played"] = duration_played + duration_to_skip
+    # Database update
+    db[chat_id][0]["played"] = to_seek
         
     await mystic.edit_text(
         text=_["admin_25"].format(seconds_to_min(to_seek), message.from_user.mention),
