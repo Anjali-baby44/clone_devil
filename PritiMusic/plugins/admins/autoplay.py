d2d8ede0b0440f83e99d3e1b81712acbc5d7d957
THE SHIV <shivofficial004@gmail.com>
2026-06-15 14:13:19 +0530

    Update autoplay.py

--- PritiMusic/plugins/admins/autoplay.py
+++ PritiMusic/plugins/admins/autoplay.py
@@ -1,5 +1,6 @@
+import random
 from pyrogram import filters
-from pyrogram.enums import ChatMemberStatus
+from pyrogram.enums import ChatMemberStatus, ButtonStyle
 from pyrogram.types import (
     Message,
     InlineKeyboardMarkup,
@@ -18,9 +19,15 @@ from PritiMusic.utils.database.autoplay import (
 from PritiMusic.utils.decorators import AdminRightsCheck
 from config import BANNED_USERS
 
-
 PHOTO_URL = "https://files.catbox.moe/6r97s4.jpg"
 
+# 🔥 PREMIUM EMOJIS LIST 🔥
+PREMIUM_EMOJIS = [
+    "5422831825178206894", 
+    "5368324170673489600",
+    "5206607081334906820",
+    "5206380668048496464"
+]
 
 def get_panel(chat_id, enabled):
     status = "🟢 𝐄ɴᴀʙʟᴇᴅ" if enabled else "🔴 𝐃ɪsᴀʙʟᴇᴅ"
@@ -41,18 +48,24 @@ def get_panel(chat_id, enabled):
         [
             [
                 InlineKeyboardButton(
-                    "🟢 𝐄ɴᴀʙʟᴇ",
+                    "𝐄ɴᴀʙʟᴇ",
                     callback_data=f"AUTOPLAY_ENABLE|{chat_id}",
+                    style=ButtonStyle.SUCCESS,
+                    icon_custom_emoji_id=random.choice(PREMIUM_EMOJIS)
                 ),
                 InlineKeyboardButton(
-                    "🔴 𝐃ɪsᴀʙʟᴇ",
+                    "𝐃ɪsᴀʙʟᴇ",
                     callback_data=f"AUTOPLAY_DISABLE|{chat_id}",
+                    style=ButtonStyle.DANGER,
+                    icon_custom_emoji_id=random.choice(PREMIUM_EMOJIS)
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    f"⚡ 𝐀ᴜᴛᴏ 𝐏ʟᴀʏ : {status}",
+                    f"𝐀ᴜᴛᴏ 𝐏ʟᴀʏ : {status}",
                     callback_data="AUTOPLAY_STATUS",
+                    style=ButtonStyle.PRIMARY,
+                    icon_custom_emoji_id=random.choice(PREMIUM_EMOJIS)
                 )
             ],
         ]
